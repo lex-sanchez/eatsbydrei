3ea8d98bcc8fb0fd809e0e9ced301c8e9011d6bd
Alex Sanchez <alex@robinhealthcare.com>
2020-04-15 19:25:13 -0700

    init

---
+++ src/components/Landing.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../styles/landing.css';
+
+const Landing = props => {
+  return (
+      <div className="landing-container" >
+          <div className="landing-header">
+              <div className="title">
+                <h1>Eats By Drei</h1>
+                  <hr></hr>
+              </div>
+            <h3>Cook. Photograph. Repeat.</h3>
+          </div>
+      </div>
+  )
+};
+
+export default Landing;
+
