87414cfc5524f35f0918139ad2edfe7cc32f3d53
Alex Sanchez <alex@robinhealthcare.com>
2020-04-18 21:49:13 -0700

    add react router routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import Landing from './components/Landing';
 import './App.css';
 
-function App() {
-  return (
-    <Landing />
-  )
+class App extends React.Component {
+  render() {
+    return (
+        <Router>
+          <Switch>
+            <Route exact path="/" render={() => <Landing />} />
+            <Route exact path="/about" render={() => 'This the about page'} />
+          </Switch>
+        </Router>
+    )
+  }
 }
 
 export default App;
